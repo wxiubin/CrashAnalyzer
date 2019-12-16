44b802bb190abdd3572fbe1d0ba5f2f2d4857c5b
王修斌 <iosgg@outlook.com>
2019-12-16 17:36:03 +0800

    Initial Commit

---
+++ crasher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  crasher
+//
+//  Created by wxiubin on 2019/12/16.
+//  Copyright © 2019 wxiubin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
