d6c716cb2bd66bffbd32b28a84cd4f906a40f03c
王修斌 <iosgg@outlook.com>
2019-12-17 21:56:19 +0800

    [RD] rename product and project name

--- CrashAnalyzer/MainViewController.swift
+++ CrashAnalyzer/MainViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  crasher
+//  CrashAnalyzer
 //
 //  Created by wxiubin on 2019/12/16.
 //  Copyright © 2019 wxiubin. All rights reserved.
@@ -16,6 +16,18 @@ class MainViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet private weak var parserButton: NSButton!
     
     @IBOutlet private weak var resultView: NSScrollView!
+    
+    var logFielPath: String? {
+        didSet {
+            logFileTextField.stringValue = logFielPath ?? ""
+        }
+    }
+    
+    var dsymPath: String? {
+        didSet {
+            dsymTextField.stringValue = dsymPath ?? ""
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +49,26 @@ class MainViewController: NSViewController, NSTextFieldDelegate {
     }
     
     @IBAction func _clicked(_ sender: NSClickGestureRecognizer) {
-    
+        
+        let isLogFile = sender.view == logFileTextField
+        
+        let textField = isLogFile ? logFileTextField : dsymTextField
+        
+        
+        let openChannel = NSOpenPanel()
+        openChannel.title = isLogFile ? "选择日志文件" : "选择符号表文件"
+        openChannel.begin { (respone) in
+            if respone.rawValue == 0 {
+                print("取消选择");
+                return
+            } else {
+                print("选中文件：\(String(describing: openChannel.urls.first))")
+            }
+            let filePath = openChannel.urls.first?.absoluteString
+            if filePath?.isEmpty == false {
+                textField?.stringValue = filePath!
+            }
+        }
     }
     
     @IBAction func _parse(_ sender: NSButton) {
